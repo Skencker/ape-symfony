bc09a8bf32b1537f65c7315561f13637327183de
Skencker <Sandrinekencker@hotmail.com>
2021-10-02 21:16:39 +0200

    dernier commit

--- src/Controller/Admin/ActualitesCrudController.php
+++ src/Controller/Admin/ActualitesCrudController.php
@@ -5,9 +5,9 @@ namespace App\Controller\Admin;
 use App\Entity\Actualites;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use Twig\Extra\Intl\IntlExtension;
 
 
